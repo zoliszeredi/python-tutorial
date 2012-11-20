3a7544cbb94cfc6d560288d156670c273500d90b
Szeredi Zoltan <zoltan.szeredi@3pillarglobal.com>
2012-11-20 17:19:05 +0200

    singleton blues

--- advanced/p3.py
+++ advanced/p3.py
@@ -6,24 +6,40 @@ import unittest
 
 class Highlander(type):
     """There can be only one"""
-    _instance = None
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super(Highlander, cls).__new__(cls, 
-                                                           *args, 
-                                                           **kwargs)
-        return cls._instance
+    def __init__(metaclass, name, bases, dictionary):
+        super(Highlander, metaclass).__init__(name,
+                                              bases,
+                                              dictionary)
+        metaclass.instance = None
+
+    def __call__(metaclass, *args, **kwargs):
+        if metaclass.instance is None:
+            metaclass.instance = super(Highlander, 
+                                       metaclass).__call__(*args, 
+                                                            **kwargs)
+        return metaclass.instance
     
 class MacLeod(object):
-    pass
+    __metaclass__ = Highlander
+    def method(self):
+        print "i am of the MacLeod clan"
+
+class Kell(object):
+    __metaclass__ = Highlander
+    def method(self):
+        print "i am of the Kell clan"
 
 class SingletonTest(unittest.TestCase):
     def setUp(self):
-        self.duncan = Highlander(MacLeod)
-        self.connor = Highlander(MacLeod)
+        self.duncan = MacLeod()
+        self.connor = MacLeod()
+        self.jacob = Kell()
 
-    def test_singleton(self):
+    def test_first_type(self):
         self.assertEquals(self.duncan, self.connor)
+
+    def test_second_type(self):
+        self.assertNotEqual(self.duncan, self.jacob)
 
 if __name__ == '__main__':
     unittest.main()
